5956eb404442f7699c36261d81bd383b1b1a153f
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-02-07 11:54:16 +0000

    providing basic grammer corrections

--- src/App.js
+++ src/App.js
@@ -5,20 +5,42 @@ export default () => {
 
   const [name, setName] = useState('keyboardbreaker');
   const [age, setAge] = useState(11);
+
+  let introText = ""; 
+  let seperator = "";
+  if(name != ""){ 
+    introText = "Hi, I am ";
+    seperator = ",";
+  }
+  else {
+    introText = "I am";
+  }
+
   return (
       <div className="App">
-        <h1>About Me Paragraph Generator</h1>
+        <h1>About Me Generator</h1>
         <div className="container">
             <div className="column">
-              <h1 className="heading">Form:</h1>
-              <p>Name: <input type="text" value={name} onChange={e => setName(e.target.value)}></input></p>
-              <p>Age: {age}</p>
-              <button onClick={() => {setAge(age + 1)}}>+ 1</button>
-              <button onClick={() => {setAge(age - 1)}}>- 1</button>
+              <h1 className="heading">Form</h1>
+              <div className="form">
+                <p>
+                  <b>Name:</b> 
+                  <input 
+                    type="text" 
+                    value={name} 
+                    onChange={e => setName(e.target.value)}>
+                  </input>
+                </p>
+                <p><b>Age:</b> {age} {' '}
+                <button onClick={() => {setAge(age + 1)}}>+ 1</button>
+                <button onClick={() => {setAge(age - 1)}}>- 1</button>
+                </p>
+              </div>
+              
             </div>
             <div className="column">
-              <h1 className="heading">Preview:</h1>
-              <p>{name}, {age}</p>
+              <h1 className="heading">Preview</h1>
+              <p>{introText} {name}{seperator} {age} years old</p>
             </div>
         </div>
       </div>
