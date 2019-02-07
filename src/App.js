1ba2952d6be52ca2da681237c12627c9ce96d386
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-02-07 02:05:11 +0000

    adding useState hook to change state of name and age

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export default () => {
+
+  const [name, setName] = useState('keyboardbreaker');
+  const [age, setAge] = useState(11);
   return (
       <div className="App">
         <header className="App-header">
-            <p>Name: <input type="text"></input></p>
-            <p>Age: </p>
-            <button>Increment Age +1</button>
+            <p>Name: <input type="text" value={name} onChange={e => setName(e.target.value)}></input></p>
+            <p>Age: {age}</p>
+            <button onClick={() => {setAge(age + 1)}}>+ 1</button>
+            <button onClick={() => {setAge(age - 1)}}>- 1</button>
+            <h1>Preview:</h1>
+            <p>{name}, {age}</p>
         </header>
       </div>
   );
