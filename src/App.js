8c08557d7eaf76b4863bc677ac6baf238463f2cb
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-02-07 11:20:58 +0000

    organising basic layout

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ export default () => {
   const [age, setAge] = useState(11);
   return (
       <div className="App">
-        <header className="App-header">
-            <p>Name: <input type="text" value={name} onChange={e => setName(e.target.value)}></input></p>
-            <p>Age: {age}</p>
-            <button onClick={() => {setAge(age + 1)}}>+ 1</button>
-            <button onClick={() => {setAge(age - 1)}}>- 1</button>
-            <h1>Preview:</h1>
-            <p>{name}, {age}</p>
-        </header>
+        <h1>About Me Paragraph Generator</h1>
+        <div className="container">
+            <div className="column">
+              <h1 className="heading">Form:</h1>
+              <p>Name: <input type="text" value={name} onChange={e => setName(e.target.value)}></input></p>
+              <p>Age: {age}</p>
+              <button onClick={() => {setAge(age + 1)}}>+ 1</button>
+              <button onClick={() => {setAge(age - 1)}}>- 1</button>
+            </div>
+            <div className="column">
+              <h1 className="heading">Preview:</h1>
+              <p>{name}, {age}</p>
+            </div>
+        </div>
       </div>
   );
 }
